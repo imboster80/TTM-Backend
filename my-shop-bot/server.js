a3cb7d062af0b4dbcc30600d95d90836c208cbc8
imboster80 <imboster80@gmail.com>
2026-08-09 22:31:46 +0630

    Update server.js

--- my-shop-bot/server.js
+++ my-shop-bot/server.js
@@ -12,6 +12,11 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB Connected Successfully!'))
   .catch((err) => console.log('Connection Failed:', err));
 
+// Cron-job လှမ်းခေါ်တဲ့အခါ Log အများကြီး မထွက်အောင် သီးသန့် route ထည့်ခြင်း
+app.get('/ping', (req, res) => {
+    res.status(200).send('pong');
+});
+
 app.get('/', (req, res) => {
     res.send('Shop Bot Server is running!');
 });
